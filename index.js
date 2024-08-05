726f03b762a45ce10e302be410d32f781efb01a4
Davi <0341907@adm.feevale.br>
2024-08-05 17:45:14 -0300

    build: add javascript

--- index.js
+++ index.js
@@ -2,7 +2,45 @@ const registerButton = document.getElementById('register-button');
 
 registerButton.addEventListener('click', makeList);
 
-let index = 0;
+const daysInMonth = (month, year) => new Date(year, month + 1, 0).getDate();
+
+let nameMonths = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+let nameDaysOfWeek = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+
+const table = document.getElementById('list-items');
+const th = document.querySelectorAll('item-header');
+const td = document.getElementsByTagName('td');
+
+const currentDate = new Date();
+let currentDay = currentDate.getDate();
+let numberDay = currentDate.getDay();
+let currentMonth = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
+
+const nameDay = nameDaysOfWeek[numberDay];
+console.log(nameDay)
+
+// getDate() = número do dia (no caso hoje é dia 05, então vai retornar 5)
+// getDay() = número do dia da semana (0 = domingo, 1 = segunda...)
+
+function makeDays() {
+    let dayCounter = 1;
+    for(let i = 0; i < td.length; i++){
+        td[i].innerText = '';
+    }
+
+    const firstDay = new Date(currentYear, currentMonth, 1).getDay();
+
+    console.log(firstDay)
+
+    for(let i = firstDay; i < firstDay + daysInMonth(currentMonth, currentYear); i++){
+        if(i < td.length){
+            td[i].innerText = dayCounter++;
+        }
+    }
+}
+
+makeDays();
 
 function makeList(ev) {
     ev.preventDefault();
@@ -10,28 +48,24 @@ function makeList(ev) {
     const textArea = document.getElementById('textAreaTask').value;
     const startDateInput = document.getElementById('startDate').value;
     const endDateInput = document.getElementById('endDate').value;
-    const listItems = document.getElementById('list-items');
 
     const startDate = new Date(startDateInput + 'T00:00:00');
     const endDate = new Date(endDateInput + 'T00:00:00');
 
-    function formatDateToBrazilian(date) {
-        const day = String(date.getDate()).padStart(2, '0');
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const year = date.getFullYear();
-
-        return `${day}/${month}/${year}`;
-    };
-
     const formattedStartDate = formatDateToBrazilian(startDate);
     const formattedEndDate = formatDateToBrazilian(endDate);
 
-    const li = document.createElement('li');
-    const rowIndex = index++;
-    li.id = 'task-' + rowIndex;
-    li.className = 'task';
-    li.innerText = 'Task: ' + textArea + '\n' + 'Start date: ' + formattedStartDate + '\n' + 'End date: ' + formattedEndDate;
+    td.innerText = 'Task: ' + textArea + '\n' + 'Start date: ' + formattedStartDate + '\n' + 'End date: ' + formattedEndDate + '\n\n';
 
-    listItems.appendChild(li);
+    table.appendChild(td);
 
-}
+};
+
+function formatDateToBrazilian(date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+};
+
