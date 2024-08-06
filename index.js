3160bf6bf35c9778083dffd4d70caca5075a0b97
Davi <0341907@adm.feevale.br>
2024-08-06 18:18:38 -0300

    build: add javascript

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ let nameDaysOfWeek = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira'
 const table = document.getElementById('list-items');
 const th = document.querySelectorAll('item-header');
 const td = document.getElementsByTagName('td');
+const tbody = document.getElementById('tbody');
+let header = document.getElementById('header');
 
 const currentDate = new Date();
 let currentDay = currentDate.getDate();
@@ -17,15 +19,14 @@ let numberDay = currentDate.getDay();
 let currentMonth = currentDate.getMonth();
 let currentYear = currentDate.getFullYear();
 
-const nameDay = nameDaysOfWeek[numberDay];
-console.log(nameDay)
+header.innerText = `${nameMonths[currentMonth]} - ${currentYear}`
 
 // getDate() = número do dia (no caso hoje é dia 05, então vai retornar 5)
 // getDay() = número do dia da semana (0 = domingo, 1 = segunda...)
 
 function makeDays() {
     let dayCounter = 1;
-    for(let i = 0; i < td.length; i++){
+    for (let i = 0; i < td.length; i++) {
         td[i].innerText = '';
     }
 
@@ -33,14 +34,70 @@ function makeDays() {
 
     console.log(firstDay)
 
-    for(let i = firstDay; i < firstDay + daysInMonth(currentMonth, currentYear); i++){
-        if(i < td.length){
+    for (let i = firstDay; i < firstDay + daysInMonth(currentMonth, currentYear); i++) {
+        if (i < td.length) {
             td[i].innerText = dayCounter++;
         }
     }
 }
 
 makeDays();
+
+function renderCalendar() {
+    const firstDay = new Date(currentYear, currentMonth, 1).getDay();
+    const totalDays = daysInMonth(currentMonth, currentYear)
+    let dayCounter = 1;
+
+    for (let i = 0; i < td.length; i++) {
+        td[i].innerText = '';
+    }
+
+    for (let i = firstDay; i < firstDay + totalDays; i++) {
+        if (i < td.length) {
+            td[i].innerText = dayCounter++;
+        }
+    }
+}
+
+if(td.length > 35){
+    const newTr = document.createElement('tr');
+    for(let i = 0; i <= 6; i++){
+        let newTd = document.createElement('td');
+        newTd.setAttribute('class', 'item border-collapse border border-slate-900');
+        newTr.appendChild(newTd);
+    }
+    tbody.appendChild(newTr);
+}
+
+function updateHeader() {
+    header.innerText = `${nameMonths[currentMonth]} - ${currentYear}`;
+}
+
+const arrowForward = document.getElementById('arrow-forward').addEventListener('click', function (ev) {
+    ev.preventDefault();
+
+    currentMonth++
+    if (currentMonth > 11) {
+        currentMonth = 0;
+        currentYear++;
+    }
+
+    updateHeader();
+    renderCalendar();
+})
+
+const arrowBack = document.getElementById('arrow-back').addEventListener('click', function (ev) {
+    ev.preventDefault();
+
+    currentMonth--
+    if (currentMonth < 0) {
+        currentMonth = 11;
+        currentYear--;
+    }
+
+    updateHeader();
+    renderCalendar();
+})
 
 function makeList(ev) {
     ev.preventDefault();
